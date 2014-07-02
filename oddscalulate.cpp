9bf6ea7034b2e7c17e3230e68c0e998b8dcfabe2
crcanfly11 <185830862@qq.com>
2014-07-02 17:28:10 +0800

    odds
    odds prog

--- oddscalulate.cpp
+++ oddscalulate.cpp
@@ -1,64 +1,126 @@
-//
-//  oddscalulate.cpp
-//  odds
-//
-//  Created by 金成日 on 14-7-1.
-//  Copyright (c) 2014年 金成日. All rights reserved.
-//
-
 #include "oddscalulate.h"
+
+fixtures_base_odds::fixtures_base_odds(double win_odds, double shake_hands_odds, double lose_odds, const char* home_team_name, const char* away_team_name)
+{
+	team_odds_[home_team_win] = win_odds;
+	team_odds_[shake_hands] = shake_hands_odds;
+	team_odds_[away_team_win] = lose_odds;
+        
+	strcpy_s(home_team_, home_team_name);
+	strcpy_s(away_team_, away_team_name);
+};
+    
+double fixtures_base_odds::operator[] (int index) 
+{
+	if(index < max_odds_type) 
+		return team_odds_[index];
+	else 
+		return -1;
+};
+
+//-----------------------------------------------------------------------
+
+forecas_result::forecas_result(double odds, const char* result_msg) : odds_(odds), multiple_(0)
+{
+	strcpy_s(result_msg_, sizeof(result_msg_), result_msg);
+	yield_ = (odds - 1)*100;
+};
+
+//-----------------------------------------------------------------------
+
+organizer::organizer() : htwin_(0), sh_(0), atwin_(0)
+{
+    memset(htname_, 0, sizeof(htname_));
+    memset(atname_, 0, sizeof(atname_));
+	memset(tmp_, 0, sizeof(tmp_));
+	memset(result_, 0, sizeof(result_));
+        
+    init();
+};
 
 void organizer::init()
 {
-    while(1) {
-        cout<<"add new fixtures odds?(Y:yes/others:no)"<<endl;
-        cin.getline(tmp_,100);
-        cin.clear();
-        
-        if(!strcmp(tmp_,"Y")||!strcmp(tmp_,"y")) {
-            cout<<"input home team win odds:"<< endl;
-            cin.getline(tmp_,100);
-            htwin_ = atof(tmp_);
-            cin.clear();
-            
-            cout<<"input shake hands odds:"<< endl;
-            cin.getline(tmp_,100);
-            sh_ = atof(tmp_);
-            cin.clear();
-            
-            cout<<"input away team win odds:"<< endl;
-            cin.getline(tmp_,100);
-            atwin_ = atof(tmp_);
-            cin.clear();
-            
-            cout<<"input away team win odds:"<< endl;
-            cin>>htname_;
-            cin.clear();
-            
-            cout<<"input away team win odds:"<< endl;
-            cin>>atname_;
-            cin.clear();
-            
+	char s1[20], s2[20], s3[20];
+
+    while(1) {     
+		cout<< "add new fixtures odds?(Y:yes/others:no)"<<endl;
+		cin>> tmp_;
+		cin.clear();
+		
+		if(!strcmp(tmp_, "Y") || !strcmp(tmp_, "y")) {
+            cout<< "input fixtures odds and team names."<< endl;
+			cin>> skipws>> s1>> s2>> s3>> htname_>> atname_;
+			htwin_ = atof(s1);
+			sh_ = atof(s2);
+			atwin_ = atof(s3);
+			cin.clear();
+
             fixtures_base_odds* pbo = new fixtures_base_odds(htwin_, sh_, atwin_, htname_, atname_);
             base_odds_.push_back(*pbo);
-        }else {
-            break;
         }
+		else
+			break;
     }
+
+	if(base_odds_.size() < 2) return;
     
     forecas_calculate(base_odds_.begin(),base_odds_.end());
+
+	cout<< "every case of results."<< endl;
+	for_each(forecas_results_.begin(), forecas_results_.end(), boost::bind(&organizer::print, this, _1));
+
+	cout<< "How to buy? all in without loss or buy Specify combination."<< endl;
+
+};
+
+void organizer::print(forecas_result_pair rpair)
+{
+	std::cout<< rpair.first<< ". "<<rpair.second.get_result_msg()<< ": "<< rpair.second.get_result_odds()<< endl;
 };
 
 void organizer::forecas_calculate(base_odds_vector::iterator begin, base_odds_vector::iterator end)
 {
-    if(begin + 1 == end) return;
+	if(begin == end) return;
+
+	base_odds_vector::iterator second = begin + 1;    
+    for_each(second, end, boost::bind(&organizer::set_forecas_result_map, this, *begin, _1));
     
-    for_each(begin, end, boost::bind(&organizer::set_forecas_result_map, this, *begin, _1));
-    
-    return forecas_calculate(++begin, end);
+    return forecas_calculate(second, end);
 };
 
-void organizer::set_forecas_result_map(int frontodd, int value)
+void organizer::set_forecas_result_map(fixtures_base_odds first, fixtures_base_odds second)
 {
-    //add this method
+	unsigned int num=0;
+	for(int i=0; i< max_odds_type; ++i) {
+		for(int j=0; j< max_odds_type; ++j) {
+			result_msg(i, j);
+			forecas_result* frt = new forecas_result(((first[i])*(second[j])), result_);
+			
+			num++;
+			forecas_results_.insert(forecas_result_pair(num, *frt));
+		}
+	}
 };
+
+void organizer::result_msg(int first, int second)
+{
+	memset(result_, 0, sizeof(result_));
+	strcat_s(result_,sizeof(result_),get_result_flag(first));
+	strcat_s(result_,sizeof(result_),get_result_flag(second));
+};
+
+const char* organizer::get_result_flag(int index)
+{
+	switch(index) {
+	case 0:
+		return "HW";
+	case 1:
+		return "SH";
+	case 2:
+		return "HL";
+	default:
+		return "N";
+	}
+};
+
+//-----------------------------------------------------------------------
--- oddscalulate.h
+++ oddscalulate.h
@@ -1,11 +1,3 @@
-//
-//  oddscalulate.h
-//  odds
-//
-//  Created by 金成日 on 14-7-1.
-//  Copyright (c) 2014年 金成日. All rights reserved.
-//
-
 #ifndef __odds__oddscalulate__
 #define __odds__oddscalulate__
 
@@ -20,6 +12,10 @@
 
 using namespace std;
 
+enum team_odd_type { max_odds_type = 3 };
+
+//-----------------------------------------------------------------------
+
 class fixtures_base_odds
 {
 	enum team_odd_enum { home_team_win = 0, shake_hands = 1, away_team_win = 2 };
@@ -29,66 +25,73 @@ class fixtures_base_odds
 	char home_team_[30];
 	char away_team_[30];
 public:
-	fixtures_base_odds(double win_odds, double shake_hands_odds, double lose_odds, const char* home_team_name, const char* away_team_name)
-	{
-		team_odds_[home_team_win] = win_odds;
-		team_odds_[shake_hands] = shake_hands_odds;
-		team_odds_[away_team_win] = lose_odds;
-        
-		std::strcpy(home_team_, home_team_name);
-		std::strcpy(away_team_, away_team_name);
-	};
+	fixtures_base_odds(double win_odds, double shake_hands_odds, double lose_odds, const char* home_team_name, const char* away_team_name);
     
+	double operator[] (int index);
+
 	double get_win_odds() const { return team_odds_[home_team_win]; }
 	double get_shake_hands_odds() const { return team_odds_[shake_hands]; }
 	double get_lose_odds() const { return team_odds_[away_team_win]; }
+
+	const char* get_home_team_name() { return home_team_; }
+	const char* get_away_team_name() { return away_team_; }
 };
+
+//-----------------------------------------------------------------------
 
 class forecas_result
 {
-	float odds_;
-	unsigned short multiple_;
-	float yield_;			     //Unit of Measure %
-	char result_msg_[50];
+	double odds_;
+	unsigned short multiple_;	 //bei shu
+	double yield_;			     //Unit of Measure %
+	char result_msg_[10];
     
 public:
-	forecas_result(float odds, const char* result_msg) : odds_(odds)
-	{
-		std::strcpy(result_msg_, result_msg);
-		yield_ = (odds - 1)*100;
-	}
+	forecas_result(double odds, const char* result_msg);
     
 	void set_result_multiple(unsigned short multiple) { multiple_ = multiple; }
-    
 	unsigned short get_result_multiple() const { return multiple_; }
-	float get_result_yield() const { return yield_; }
+	double get_result_odds() const { return odds_; }
+	double get_result_yield() const { return yield_; }
 	const char* get_result_msg() const { return result_msg_; }
 };
+
+//-----------------------------------------------------------------------
+
+class position
+{
+	char earnings_range[100];   //range of yield
+	double cost;
+};
+
+//-----------------------------------------------------------------------
 
 class organizer
 {
 	double htwin_, sh_, atwin_;
-	char htname_[20], atname_[20], tmp_[100];
+	char htname_[20], atname_[20], tmp_[100], result_[5];
     
 	typedef vector<fixtures_base_odds> base_odds_vector;
 	base_odds_vector base_odds_;
     
-	typedef map<unsigned short, forecas_result> forecas_result_map;
+	typedef map<unsigned int, forecas_result> forecas_result_map;
 	forecas_result_map forecas_results_;
     
+	typedef pair<unsigned int, forecas_result> forecas_result_pair;
+
 public:
-    organizer() : htwin_(0), sh_(0), atwin_(0)
-    {
-        memset(htname_, 0, 20);
-        memset(atname_, 0, 20);
-        
-        init();
-    };
+    organizer();
     
 private:
     void init();
     void forecas_calculate(base_odds_vector::iterator begin, base_odds_vector::iterator end);
-    void set_forecas_result_map(int frontodd, int value);
+    void set_forecas_result_map(fixtures_base_odds first, fixtures_base_odds second);
+	void result_msg(int first, int second);
+	const char* get_result_flag(int index);
+
+	void print(forecas_result_pair rpair);
 };
+
+//-----------------------------------------------------------------------
 
 #endif /* defined(__odds__oddscalulate__) */
